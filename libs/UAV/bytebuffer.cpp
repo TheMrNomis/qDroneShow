dc38d25aab9dbe108cd36356d1ee7198ecec51cb
TheMrNomis <n0m1s@homnomnom.fr>
2015-06-11 14:38:03 +0200

    GPL disclaimer

--- libs/UAV/bytebuffer.cpp
+++ libs/UAV/bytebuffer.cpp
@@ -1,3 +1,22 @@
+/*=======================================================================
+ * qDroneShow Aerial vehicle show planification
+ *
+ * Copyright (C) 2015 - Amaury Louarn <amaury.louarn@homnomnom.fr>
+ *
+ * qDroneShow is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Feneral public license as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qDroneShow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without event the implied warranty of
+ * MERCHANDABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qDroneShow. If not, see <http://www.gnu.org/licenses/>
+ * ======================================================================*/
+
 #include "bytebuffer.h"
 
 ByteBuffer::ByteBuffer(bool bytesSwap, bool alignedFields):
