8206db0028a1ee47f392f474fe5a7739bf9a739a
TheMrNomis <n0m1s@homnomnom.fr>
2015-06-17 14:51:37 +0200

    Beginning of UAV implementation

--- libs/UAV/uav.cpp
+++ libs/UAV/uav.cpp
@@ -19,88 +19,321 @@
 
 #include "uav.h"
 
-UAV::UAV(QObject *parent) : QObject(parent)
+UAV::UAV(uint8_t UAVsystemId, uint8_t GCSsystemId, uint8_t UAVcomponentId, uint8_t GCScomponentId, QObject *parent):
+  QObject(parent),
+  m_links(),
+
+  //basic infos
+  m_name(""),
+  m_sequenceNumber(1),
+  m_type(MAV_TYPE_GENERIC),
+  m_airframe(),
+  m_autopilot(MAV_AUTOPILOT_GENERIC),
+  m_systemIsArmed(false),
+  m_baseMode(0),
+  m_customMode(0),
+  m_status(-1),
+  //m_shortModeText(),  //NOT INITIALIZED
+  //m_shortStateText(), //NOT INITIALIZED
+
+  m_UAVsystemID(UAVsystemId),
+  m_GCSsystemID(GCSsystemId),
+  m_UAVcomponentID(UAVcomponentId),
+  m_GCScomponentID(GCScomponentId),
+
+  //heartbeats
+  m_heartbeatsEnabled(),
+  m_lastHeartbeatTimestamp(),
+
+  //battery
+  startVoltage(-1.0f),
+  tickVoltage(10.5f),
+  lastTickVoltageValue(13.0f),
+  tickLowpassVoltage(12.0f),
+  warnLevelPercent(20),
+  currentVoltage(12.6f),
+  lpVoltage(12.0f),
+  currentCurrent(0.4f),
+  //batteryRemainingEstimateEnabled(),
+  chargeLevel(-1),
+  timeRemaining(0),
+  lowBattAlarm(false),
+
+  //time
+  m_startTime(),//TODO
+  m_onBoardTimeOffset(0),
+
+  //position
+  m_positionLock(false),
+  m_localPositionKnown(false),
+  m_globalPositionKnown(false),
+  m_satelliteCount(0),
+  m_globalEstimatorActive(false),
+
+  m_localX(0.0),
+  m_localY(0.0),
+  m_localZ(0.0),
+
+  m_latitude(0.0),
+  m_longitude(0.0),
+  m_altitudeAMSL(0.0),
+  m_altitudeRelative(0.0),
+
+  m_GPSLatitude(0.0),
+  m_GPSLongitude(0.0),
+  m_GPSAltitude(0.0),
+
+  m_speedX(0.0),
+  m_speedY(0.0),
+  m_speedZ(0.0),
+
+  //waypoints
+  //m_distToWaypoint(),
+  m_airspeed(std::numeric_limits<double>::quiet_NaN()),
+  m_groundSpeed(std::numeric_limits<double>::quiet_NaN()),
+  //m_bearingToWaypoint(),
+
+  //attitude
+  m_attitudeKnown(false),
+  m_attitudeStamped(false),
+  m_lastAttitudeTimestamp(0),
+
+  m_roll(0.0),
+  m_pitch(0.0),
+  m_yaw(0.0)
 {
 
+  emit disarmed();
+  emit armingChanged(false);
 }
 
-double UAV::getLocalX() const
+void UAV::addLink(Link* link)
 {
-  //TODO
-  return 0;
-}
-double UAV::getLocalY() const
-{
-  //TODO
-  return 0;
-}
-double UAV::getLocalZ() const
-{
-  //TODO
-  return 0;
-}
-bool UAV::localPositionKnown() const
-{
-  //TODO
-  return false;
+  m_links.push_back(link);
+  connect(link, SIGNAL(bytesReceived(ByteBuffer)), this, SLOT(receiveBytes(ByteBuffer)));
 }
 
-double UAV::getLatitude() const
+void launch();
+/** @brief Write this waypoint to the list of waypoints */
+//void UAV::setWaypoint(Waypoint* wp); FIXME tbd
+/** @brief Set currently active waypoint */
+//void UAV::setWaypointActive(int id); FIXME tbd
+/** @brief Order the robot to return home **/
+void UAV::home()
 {
   //TODO
-  return 0;
-}
-double UAV::getLongitude() const
-{
-  //TODO
-  return 0;
-}
-double UAV::getAltitudeAMSL() const
-{
-  //TODO
-  return 0;
-}
-double UAV::getAltitudeRelative() const
-{
-  //TODO
-  return 0;
-}
-bool UAV::globalPositionKnown() const
-{
-  //TODO
-  return 0;
 }
 
-double UAV::getRoll() const
+/** @brief Order the robot to land **/
+void UAV::land()
 {
   //TODO
-  return 0;
-}
-double UAV::getPitch() const
-{
-  //TODO
-  return 0;
-}
-double UAV::getYaw() const
-{
-  //TODO
-  return 0;
 }
 
-bool UAV::isArmed() const
+/** @brief Order the robot to pair its receiver **/
+void UAV::pairRX(int rxType, int rxSubType)
 {
   //TODO
-  return false;
 }
 
-//void UAV::executeCommand(MAV_CMD command, int confirmation = 0, float param1 = 0.0f, float param2 = 0.0f, float param3 = 0.0f, float param4 = 0.0f, float param5 = 0.0f, float param6 = 0.0f, float param7 = 0.0f, int component = 0)
-//{
+void UAV::halt()
+{
+  //TODO
+}
 
-//}
+void UAV::go()
+{
+  //TODO
+}
+
+/** @brief Stops the robot system. If it is an MAV, the robot starts the emergency landing procedure **/
+void UAV::emergencySTOP()
+{
+  //TODO
+}
+
+/** @brief Kills the robot. All systems are immediately shut down (e.g. the main power line is cut). This might lead to a crash **/
+bool UAV::emergencyKILL()
+{
+  //TODO
+}
+
+/** @brief Shut the system cleanly down. Will shut down any onboard computers **/
+void UAV::shutdown()
+{
+  //TODO
+}
+
+/** @brief Set the target position for the robot to navigate to. */
+void UAV::setTargetPosition(float x, float y, float z, float yaw)
+{
+  //TODO
+}
+
+//low battery alarm
+void UAV::startLowBattAlarm()
+{
+  //TODO
+}
+
+void UAV::stopLowBattAlarm()
+{
+  //TODO
+}
+
+//arming
+void UAV::armSystem()
+{
+  //TODO
+}
+
+void UAV::disarmSystem()
+{
+  //TODO
+}
+
+void UAV::toggleArmedState()
+{
+  if(m_systemIsArmed)
+    disarmSystem();
+  else
+    armSystem();
+}
+
+//autonomy
+void UAV::goAutonomous()
+{
+  //TODO
+}
+
+void UAV::goManual()
+{
+  //TODO
+}
+
+void UAV::toggleAutonomy()
+{
+  //TODO
+}
+
+//messages
+void UAV::receiveMessage(MAVLinkMessage const& msg)
+{
+  //TODO
+}
+
+void UAV::receiveMessage(ByteBuffer const& msg)
+{
+  ByteBuffer buf(msg);
+  receiveMessage(MAVLinkMessage(buf));
+}
+
+void UAV::sendMessage(MAVLinkMessage const& msg)
+{
+  sendMessage(msg.toByteBuffer());
+}
+
+void UAV::sendMessage(ByteBuffer const& msg)
+{
+  //TODO
+}
+
+//mode & state
+void UAV::setMode(MAV_MODE baseMode, uint32_t customMode)
+{
+  //TODO
+}
+
+void UAV::updateState()
+{
+  //TODO : emit heartBeatTimeout
+  uint64_t heartbeatInterval = QDateTime::toTime_t() - m_lastHeartbeatTimestamp;
+  if((heartbeatInterval > HEARTBEAT_TIMEOUT))
+    m_connectionLost = true;
+  else
+    m_connectionLost = false;
+}
+
+
+//positionning
+void UAV::setLocalOriginAtCurrentGPSPosition()
+{
+  executeCommand(MAV_CMD_DO_SET_HOME,1,1,0,0,0,0,0,0);
+}
+
+void UAV::setHomePosition(double latitude, double longitude, double altitude)
+{
+  //TODO
+}
+
+void UAV::executeCommand(MAV_CMD command, int confirmation = 0, float param1 = 0.0f, float param2 = 0.0f, float param3 = 0.0f, float param4 = 0.0f, float param5 = 0.0f, float param6 = 0.0f, float param7 = 0.0f)
+{
+  execute(MAVLinkCommand(m_GCSsystemID,m_GCScomponentID,m_sequenceNumber++,m_UAVsystemID,m_UAVcomponentID,command,confirmation,param1,param2,param3,param4,param5,param6,param7));
+}
+
 
 void UAV::execute(MAVLinkMessage what)
 {
-  for(auto i = links.cbegin(); i != links.cend(); i++)
+  for(auto i = m_links.cbegin(); i != m_links.cend(); i++)
     (*i)->sendMessage(what);
 }
 
+void UAV::sendHeartbeat()
+{
+  //TODO : check if params are OK
+  execute(MAVLinkHeartbeat(m_systemID, 1, m_sequenceNumber++,m_type,m_autopilot,m_baseMode,m_customMode,m_status));
+}
+
+//getters & setters
+QString UAV::getName() const{return m_name;}
+uint8_t UAV::getSystemID() const{return m_systemID;}
+uint8_t UAV::getSequenceNumber() const{return m_sequenceNumber;}
+MAV_TYPE UAV::getType() const{return m_type;}
+MAV_FRAME UAV::getAirframe() const{return m_airframe;}
+MAV_AUTOPILOT UAV::getAutopilot() const{return m_autopilot;}
+MAV_MODE UAV::getBaseMode() const{return m_baseMode;}
+uint32_t UAV::getCustomMode() const{return m_customMode;}
+MAV_STATE UAV::getStatus() const{return m_status;}
+QString UAV::getShortModeText() const{return m_shortModeText;}
+QString UAV::getShortStateText() const{return m_shortStateText;}
+
+bool UAV::isArmed() const{return m_systemIsArmed;}
+
+uint64_t UAV::getStartTime() const{return m_startTime;}
+uint64_t UAV::getOnBoardTimeOffset() const{return m_onBoardTimeOffset;}
+
+bool UAV::isPositionLock() const{return m_positionLock;}
+bool UAV::isLocalPositionKnown() const{return m_localPositionKnown;}
+bool UAV::isGlobalPositionKnown() const{return m_globalPositionKnown;}
+double UAV::getSatelliteCount() const{return m_satelliteCount;}
+bool UAV::isGlobalEstimatorActive() const{return m_globalEstimatorActive;}
+
+double UAV::getLocalX() const{return m_localX;}
+double UAV::getLocalY() const{return m_localY;}
+double UAV::getLocalZ() const{return m_localZ;}
+
+double UAV::getLatitude() const{return m_latitude;}
+double UAV::getLongitude() const{return m_longitude;}
+double UAV::getAltitudeAMSL() const{return m_altitudeAMSL;}
+double UAV::getAltitudeRelative() const{return m_altitudeRelative;}
+
+double UAV::getGPSLatitude() const{return m_GPSAltitude;}
+double UAV::getGPSLongitude() const{return m_GPSLatitude;}
+double UAV::getGPSAltitude() const{return m_GPSLongitude;}
+
+double UAV::getSpeedX() const{return m_speedX;}
+double UAV::getSpeedY() const{return m_speedY;}
+double UAV::getSpeedZ() const{return m_speedZ;}
+
+double UAV::getDistToWaypoint() const{return m_distToWaypoint;}
+double UAV::getAirspeed() const{return m_airspeed;}
+double UAV::getGroundSpeed() const{return m_groundSpeed;}
+double UAV::getBearingToWaypoint() const{return m_bearingToWaypoint;}
+
+bool UAV::isAttitudeKnown() const{return m_attitudeKnown;}
+bool UAV::isAttitudeStamped() const{return m_attitudeStamped;}
+uint64_t UAV::getLastAttitudeTimestamp() const{return m_lastAttitudeTimestamp;}
+
+double UAV::getRoll() const{return m_roll;}
+double UAV::getPitch() const{return m_pitch;}
+double UAV::getYaw() const{return m_yaw;}
--- libs/UAV/uav.h
+++ libs/UAV/uav.h
@@ -1,4 +1,4 @@
-/*======================================================================
+/*=======================================================================
  * qDroneShow Aerial vehicle show planification
  *
  * Copyright (C) 2015 - Amaury Louarn <amaury.louarn@homnomnom.fr>
@@ -17,15 +17,27 @@
  * along with qDroneShow. If not, see <http://www.gnu.org/licenses/>
  * ======================================================================*/
 
+/*=======================================================================
+ * This file contains fragments of code from the QGroundControl project,
+ * which is released under the terms of the GNU General Public License,
+ * version 3.
+ *
+ * Copyright (C) 2009 - 2015 QGROUNDCONTROL PROJECT
+ * <http://www.qgroundcontrol.org>
+ * ======================================================================*/
+
 #ifndef UAV_H
 #define UAV_H
 
 #include <iostream>
 #include <QObject>
+#include <QDateTime>
 
-#include "mavlinkcmd.h"
 #include "mavlinkmessage.h"
+#include "mavlinkheartbeat.h"
+#include "mavlinkcommand.h"
 #include "link.h"
+#include "enums.h"
 
 /**
  * @brief This class represents one vehicle (Unmanned Aerial Vehicle)
@@ -35,37 +47,94 @@ class UAV : public QObject
 {
     Q_OBJECT
 public:
-  explicit UAV(QObject *parent = 0);
+  explicit UAV(uint8_t UAVsystemId, uint8_t GCSsystemId = 255, uint8_t UAVcomponentId = 0, uint8_t GCScomponentId = 0, QObject *parent = 0);
+  ~UAV();
 
-  virtual double getLocalX() const;
-  virtual double getLocalY() const;
-  virtual double getLocalZ() const;
-  virtual bool localPositionKnown() const;
+  virtual void addLink(Link* link);
 
-  virtual double getLatitude() const;
-  virtual double getLongitude() const;
-  virtual double getAltitudeAMSL() const;
-  virtual double getAltitudeRelative() const;
-  virtual bool globalPositionKnown() const;
+  /** @brief Launches the system **/
+  void launch();
+  /** @brief Write this waypoint to the list of waypoints */
+  //void setWaypoint(Waypoint* wp); FIXME tbd
+  /** @brief Set currently active waypoint */
+  //void setWaypointActive(int id); FIXME tbd
+  /** @brief Order the robot to return home **/
+  void home();
+  /** @brief Order the robot to land **/
+  void land();
+  /** @brief Order the robot to pair its receiver **/
+  void pairRX(int rxType, int rxSubType);
 
-  virtual double getRoll() const;
-  virtual double getPitch() const;
-  virtual double getYaw() const;
+  void halt();
+  void go();
 
-  virtual bool isArmed() const;
+  /** @brief Stops the robot system. If it is an MAV, the robot starts the emergency landing procedure **/
+  void emergencySTOP();
 
-  /** @brief Get the type of the system (airplane, quadrotor, helicopter,..)*/
-  virtual int getSystemType() = 0;
+  /** @brief Kills the robot. All systems are immediately shut down (e.g. the main power line is cut). This might lead to a crash **/
+  bool emergencyKILL();
 
-  /** @brief Get the type of the autopilot (PIXHAWK, APM, UDB, PPZ,..) */
-  virtual int getAutopilotType() = 0;
-  virtual QString getAutopilotTypeName() = 0;
-  virtual void setAutopilotType(int apType) = 0;
+  /** @brief Shut the system cleanly down. Will shut down any onboard computers **/
+  void shutdown();
+
+  /** @brief Set the target position for the robot to navigate to. */
+  void setTargetPosition(float x, float y, float z, float yaw);
+
+  //low battery alarm
+  void startLowBattAlarm();
+  void stopLowBattAlarm();
+
+  //arming
+  void armSystem();
+  void disarmSystem();
+  void toggleArmedState();
+
+  //autonomy
+  void goAutonomous();
+  void goManual();
+  void toggleAutonomy();
+
+  //messages
+  void receiveMessage(MAVLinkMessage const& msg);
+  void receiveMessage(ByteBuffer const& msg);
+  void sendMessage(MAVLinkMessage const& msg);
+  void sendMessage(ByteBuffer const& msg);
+
+  //mode & state
+  void setMode(MAV_MODE baseMode, uint32_t customMode);
+  void updateState();
+
+  //positionning
+  void setLocalOriginAtCurrentGPSPosition();
+  void setHomePosition(double latitude, double longitude, double altitude);
 
 signals:
+  /**
+   * @brief signal sent immediatly after a change of the arming state
+   * @param a boolean to indicate the new value of the armind : true if the UAV is now armed, false otherwise
+   */
+  void armingStateChanged(bool);
+
+  void localXChanged(double val,QString name);
+  void localYChanged(double val,QString name);
+  void localZChanged(double val,QString name);
+  void longitudeChanged(double val,QString name);
+  void latitudeChanged(double val,QString name);
+  void altitudeAMSLChanged(double val,QString name);
+  void altitudeAMSLFTChanged(double val,QString name);
+  void altitudeWGS84Changed(double val,QString name);
+  void altitudeRelativeChanged(double val,QString name);
+  void rollChanged(double val,QString name);
+  void pitchChanged(double val,QString name);
+  void yawChanged(double val,QString name);
+  void satelliteCountChanged(double val,QString name);
+  void distToWaypointChanged(double val,QString name);
+  void groundSpeedChanged(double val, QString name);
+  void airSpeedChanged(double val, QString name);
+  void bearingToWaypointChanged(double val,QString name);
 
 public slots:
-  //void executeCommand(MAV_CMD command, int confirmation = 0, float param1 = 0.0f, float param2 = 0.0f, float param3 = 0.0f, float param4 = 0.0f, float param5 = 0.0f, float param6 = 0.0f, float param7 = 0.0f, int component = 0);
+  void executeCommand(MAV_CMD command, int confirmation = 0, float param1 = 0.0f, float param2 = 0.0f, float param3 = 0.0f, float param4 = 0.0f, float param5 = 0.0f, float param6 = 0.0f, float param7 = 0.0f);
 
   /**
    * @brief get the UAV to execute the MAVLink order
@@ -73,11 +142,163 @@ public slots:
    */
   void execute(MAVLinkMessage what);
 
-private:
-  bool m_isArmed;
-  bool m_heartBeatsEnabled;
+  /**
+   * @brief sends a heartbeat to the UAV
+   */
+  void sendHeartbeat();
 
-  std::list<Link*> links;
+  void receiveBytes(ByteBuffer);
+
+public:
+  //getters
+  QString getName() const;
+  uint8_t getSystemID() const;
+  uint8_t getSequenceNumber() const;
+  MAV_TYPE getType() const;
+  MAV_FRAME getAirframe() const;
+  MAV_AUTOPILOT getAutopilot() const;
+  MAV_MODE getBaseMode() const;
+  uint32_t getCustomMode() const;
+  MAV_STATE getStatus() const;
+  QString getShortModeText() const;
+  QString getShortStateText() const;
+
+  virtual bool isArmed() const;
+
+  //time
+  uint64_t getStartTime() const;
+  uint64_t getOnBoardTimeOffset() const;
+
+  //position
+  bool isPositionLock() const;
+  bool isLocalPositionKnown() const;
+  bool isGlobalPositionKnown() const;
+  double getSatelliteCount() const;
+  bool isGlobalEstimatorActive() const;
+
+  double getLocalX() const;
+  double getLocalY() const;
+  double getLocalZ() const;
+
+  double getLatitude() const;
+  double getLongitude() const;
+  double getAltitudeAMSL() const;
+  double getAltitudeRelative() const;
+
+  double getGPSLatitude() const;
+  double getGPSLongitude() const;
+  double getGPSAltitude() const;
+
+  double getSpeedX() const;
+  double getSpeedY() const;
+  double getSpeedZ() const;
+
+  //waypoints
+  double getDistToWaypoint() const;
+  double getAirspeed() const;
+  double getGroundSpeed() const;
+  double getBearingToWaypoint() const;
+
+  //attitude
+  bool isAttitudeKnown() const;
+  bool isAttitudeStamped() const;
+  uint64_t getLastAttitudeTimestamp() const;
+
+  double getRoll() const;
+  double getPitch() const;
+  double getYaw() const;
+
+private:
+  //constants
+  const uint64_t HEARTBEAT_TIMEOUT;
+
+  std::list<Link*> m_links;
+
+  //basic infos
+  QString m_name;
+  uint8_t m_sequenceNumber;
+  MAV_TYPE m_type;
+  MAV_FRAME m_airframe;
+  MAV_AUTOPILOT m_autopilot;
+  bool m_systemIsArmed;
+  MAV_MODE m_baseMode;
+  uint32_t m_customMode;
+  MAV_STATE m_status;
+  QString m_shortModeText;
+  QString m_shortStateText;
+
+  //IDs
+  uint8_t m_UAVsystemID;
+  uint8_t m_GCSsystemID;
+  uint8_t m_UAVcomponentID;
+  uint8_t m_GCScomponentID;
+
+  //heartbeats
+  bool m_heartbeatsEnabled;
+  bool m_connectionLost;
+  uint64_t m_lastHeartbeatTimestamp;
+
+  //battery
+  float startVoltage;         ///< Voltage at system start
+  float tickVoltage;          ///< Voltage where 0.1 V ticks are told
+  float lastTickVoltageValue; ///< The last voltage where a tick was announced
+  float tickLowpassVoltage;   ///< Lowpass-filtered voltage for the tick announcement
+  float warnLevelPercent;     ///< Warning level, in percent
+  double currentVoltage;      ///< Voltage currently measured
+  float lpVoltage;            ///< Low-pass filtered voltage
+  double currentCurrent;      ///< Battery current currently measured
+  bool batteryRemainingEstimateEnabled; ///< If the estimate is enabled, QGC will try to estimate the remaining battery life
+  float chargeLevel;          ///< Charge level of battery, in percent
+  int timeRemaining;          ///< Remaining time calculated based on previous and current
+  bool lowBattAlarm;          ///< Switch if battery is low
+
+  //time
+  uint64_t m_startTime;
+  uint64_t m_onBoardTimeOffset;
+
+  //position
+  bool m_positionLock;
+  bool m_localPositionKnown;
+  bool m_globalPositionKnown;
+  double m_satelliteCount;
+  bool m_globalEstimatorActive;
+
+  double m_localX;
+  double m_localY;
+  double m_localZ;
+
+  double m_latitude;
+  double m_longitude;
+  double m_altitudeAMSL;
+  double m_altitudeRelative;
+
+  double m_GPSLatitude;
+  double m_GPSLongitude;
+  double m_GPSAltitude;
+
+  double m_speedX;
+  double m_speedY;
+  double m_speedZ;
+
+  //waypoints
+  double m_distToWaypoint;
+  double m_airspeed;
+  double m_groundSpeed;
+  double m_bearingToWaypoint;
+
+  //attitude
+  bool m_attitudeKnown;
+  bool m_attitudeStamped;
+  uint64_t m_lastAttitudeTimestamp;
+
+  double m_roll;
+  double m_pitch;
+  double m_yaw;
+
+  //parameters
+  bool blockHomePositionChanges;   ///< Block changes to the home position
+  bool receivedMode;          ///< True if mode was retrieved from current conenction to UAS
+
 };
 
 #endif // UAV_H
