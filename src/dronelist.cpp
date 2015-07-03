8a97b7ba9c91e101921acdc1d3c071b1ab7c49a0
TheMrNomis <n0m1s@homnomnom.fr>
2015-07-03 14:13:42 +0200

    DroneList implementation

--- src/dronelist.cpp
+++ src/dronelist.cpp
@@ -1,14 +1,41 @@
 #include "dronelist.h"
-#include "ui_dronelist.h"
 
-DroneList::DroneList(QWidget *parent) :
+DroneList::DroneList(Link* connection, QWidget *parent) :
   QGroupBox(parent),
-  ui(new Ui::DroneList)
+  m_mainLayout(new QVBoxLayout),
+  m_connection(connection),
+  m_uavWidgets()
 {
-  ui->setupUi(this);
+  QObject::connect(m_connection, SIGNAL(messageReceived(MAVLinkMessage)), this, SLOT(_receiveMessage(MAVLinkMessage)));
+
+  setLayout(m_mainLayout);
+
+  if(!m_connection->isConnected())
+    if(!m_connection->connect())
+      QMessageBox::critical(this, "Connection error", "Impossible to connect "+m_connection->getName());
 }
 
 DroneList::~DroneList()
 {
-  delete ui;
+}
+
+void DroneList::_receiveMessage(MAVLinkMessage const& msg)
+{
+  UAVWidget * UAVYoureLookingFor = nullptr;
+  const uint8_t systemID = msg.get_systemID();
+
+  //checking if we have a UAV with this system id
+  for(auto it = m_uavWidgets.begin(); UAVYoureLookingFor == nullptr && it != m_uavWidgets.end(); ++it)
+    if((*it)->getUAVSystemID() == systemID)
+      UAVYoureLookingFor = *it;
+
+  //if we don't, we create a new one
+  if(UAVYoureLookingFor == nullptr)
+  {
+    UAVYoureLookingFor = new UAVWidget(static_cast<unsigned int>(m_uavWidgets.size()), systemID, m_connection, this);
+    m_uavWidgets.push_back(UAVYoureLookingFor);
+    m_mainLayout->addWidget(UAVYoureLookingFor);
+  }
+
+  UAVYoureLookingFor->m_uav->receiveMessage(msg);
 }
--- src/dronelist.h
+++ src/dronelist.h
@@ -2,21 +2,30 @@
 #define DRONELIST_H
 
 #include <QGroupBox>
+#include <QMessageBox>
+#include <vector>
+#include "UAV/link.h"
+#include "uavwidget.h"
 
-namespace Ui {
-  class DroneList;
-}
-
+/**
+ * @brief This class is GroupBox containing UAVWidgets. It is used as an interface that creates UAVs as it receive MAVLink messages through the Link "connection"
+ * @author Amaury Louarn <amaury.louarn@homnomnom.fr>
+ */
 class DroneList : public QGroupBox
 {
   Q_OBJECT
 
 public:
-  explicit DroneList(QWidget *parent = 0);
+  explicit DroneList(Link * connection, QWidget *parent = 0);
   ~DroneList();
 
+private slots:
+  void _receiveMessage(MAVLinkMessage const& msg);
+
 private:
-  Ui::DroneList *ui;
+  Link * m_connection;
+  std::vector<UAVWidget *> m_uavWidgets;
+  QVBoxLayout * m_mainLayout;
 };
 
 #endif // DRONELIST_H
